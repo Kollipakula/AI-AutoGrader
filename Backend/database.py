7d6f36d1ebb854796c178fd9cbdb14db5fb8ad9a
Kollipakula <153719567+Kollipakula@users.noreply.github.com>
2026-05-31 10:41:42 +0530

    Secured MongoDB credentials

--- Backend/database.py
+++ Backend/database.py
@@ -1,18 +1,16 @@
-# from pymongo import MongoClient
-
-# # 🔥 Replace with your actual Atlas connection string
-# # client = MongoClient("mongodb://localhost:27017")
-
-# db = client["evaluation_db"]
-# collection = db["results"]
-
-
 from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
 
-# 🔥 Replace with your MongoDB Atlas connection string
-client = MongoClient("mongodb+srv://test-1:123@cluster0.fuwlpob.mongodb.net/")
+# Load environment variables
+load_dotenv()
 
+# MongoDB Connection
+client = MongoClient(os.getenv("MONGO_URI"))
+
+# Database
 db = client["evaluation_system"]
 
+# Collections
 exams_collection = db["exams"]
 students_collection = db["students"]
